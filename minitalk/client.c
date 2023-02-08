85eea1a3c786e0527370f110a28bbb878437f9fc
Alvina LE SAINT <ale-sain@e2r10p20.clusters.42paris.fr>
2023-02-08 20:16:11 +0100

    marche mais pas a 100% encore

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,11 +6,12 @@
 /*   By: ale-sain <ale-sain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 18:05:31 by alvina            #+#    #+#             */
-/*   Updated: 2023/02/06 19:48:26 by ale-sain         ###   ########.fr       */
+/*   Updated: 2023/02/08 19:36:33 by ale-sain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+int     g_msg_received;
 
 char	*binaire(char c)
 {
@@ -29,40 +30,58 @@ char	*binaire(char c)
 	return (octet);
 }
 
-int main(int ac, char **av)
+void	char_sender(char *octet, int pid)
 {
-    int     pid;
-    int     i;
-    char    *str;
-    char    *octet;
+	int	j;
+	int	size;
 
-    pid = ft_atoi(av[1]);
-    printf("%d\n", pid);
-    // str = av[2];
-    // i = 0;
-    // int j = 0;
-    kill(pid, SIGUSR2);
-    usleep(200);
-    // while (str[i])
-    // {
-    //     octet = binaire(str[i]);
-    //     while (octet[j])
-    //     {
-    //         if (octet[j] == '0')
-    //         {
-    //             printf("0");
-    //             kill(pid, SIGUSR1);
-    //         }
-    //         else
-    //         {
-    //             printf("1");
-    //             kill(pid, SIGUSR2);
-    //         }
-    //         j++;
-    //     }
-    //     j = 0;
-    //     free(octet);
-    //     i++;
-    // }
-    
+	j = 0;
+	size = 7;
+	g_msg_received = 0;
+	while (octet[j])
+	{
+		if (octet[size -j] == '0')
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		while (!g_msg_received)
+			pause();
+		write(1, &octet[size - j], 1);
+		j++;
+		g_msg_received = 0;
+	}
+	free(octet);
+}
+
+void	test(int signum, siginfo_t *client, void *context)
+{
+	(void)client;
+	(void)context;
+	if (signum == SIGUSR1)
+		g_msg_received = 1;
+}
+
+int	main(int ac, char **av)
+{
+	int					pid;
+	int					i;
+	char				*str;
+	char				*octet;
+	struct sigaction	sa;
+
+	pid = ft_atoi(av[1]);
+	str = av[2];
+	i = 0;
+	sa.sa_sigaction = &test;
+	sa.sa_flags = SA_RESTART;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	while (str[i])
+	{
+		octet = binaire(str[i]);
+		char_sender(octet, pid);
+		i++;
+	}
+	char_sender(binaire('\n'), pid);
+	char_sender(binaire('\0'), pid);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,23 +6,140 @@
 /*   By: ale-sain <ale-sain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 15:26:16 by alvina            #+#    #+#             */
-/*   Updated: 2023/02/06 18:12:05 by ale-sain         ###   ########.fr       */
+/*   Updated: 2023/02/08 19:37:12 by ale-sain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	test(int signum, siginfo_t *client, void *context)
+void	ft_putchar(int c)
 {
-	int	pid;
-	(void)context;
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb * ft_recursive_power(nb, power - 1));
+}
+
+int	ft_strlen(char *str)
+{
+	int i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char c)
+{
+	char	*str;
+	int		i;
+	int		j;
+	int		length;
+
+	i = 0;
+	j = 0;
+	length = ft_strlen(s1) + 1;
+	str = (char *) malloc(sizeof(char) * length + 1);
+	if (!str)
+		return (NULL);
+	if (s1)
+	{
+		while (s1[j])
+			str[i++] = s1[j++];
+	}
+	j = 0;
+	str[i++] = c;
+	str[i] = '\0';
+	return (str);
+}
+
+int	print_char(char octet[9])
+{
+	int c;
+	int	i;
+	int size;
+	int nb = 0;
+
+	c = 0;
+	i = 0;
+	size = 7;
+	while (octet[i])
+	{
+		c += ft_recursive_power(2, i) * (octet[size - i] - 48);
+		i++;
+	}
+	return (c);
+}
+
+void	ft_putnbr(int n)
+{
+	long	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+void	char_receiver(int signum, siginfo_t *client, void *context)
+{
+	int			pid;
+	(void)		context;
+	static int i;
+	static char *str;
+	int letter;
+	static char octet[9];
 
 	pid = client->si_pid;
-	printf(".");
+
 	if (signum == SIGUSR1)
-		printf("0");
-	if (signum == SIGUSR2)
-		printf("1");
+		octet[i] = '0';
+	else if (signum == SIGUSR2)
+		octet[i] = '1';
+	kill(pid, SIGUSR1);
+	i++;
+	if (i == 8)
+	{
+		i = 0;
+		octet[8] = '\0';
+		letter = print_char(octet);
+		// ft_putchar(letter);
+		// ft_putnbr(letter);
+		str = ft_strjoin(str, letter);
+		if (letter == 0)
+		{
+			ft_putstr(str);
+			free(str);
+		}
+	}
 }
 
 int	main(int ac, char **av)
@@ -30,9 +147,11 @@ int	main(int ac, char **av)
 	int					pid;
 	struct sigaction	sa;
 
-	sa.sa_sigaction = &test;
+	sa.sa_sigaction = &char_receiver;
+	sa.sa_flags = SA_SIGINFO;
 	pid = getpid();
-	printf("PID : %d\n", pid);
+	ft_putnbr(pid);
+	ft_putchar('\n');
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
@@ -40,4 +159,5 @@ int	main(int ac, char **av)
 	{
 		pause();
 	}
+	ft_putchar('\n');
 }
